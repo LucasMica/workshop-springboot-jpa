6a20022469cf7592221b24859644a66f61579cd1
Lucas <lucasmicaoliveira@gmail.com>
2026-05-14 18:42:04 -0300

    Order, instant, ISO - 1

--- src/main/java/com/javastudy/course/entities/User.java
+++ src/main/java/com/javastudy/course/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -19,6 +21,8 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -69,6 +73,11 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @OneToMany(mappedBy = "client")
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
